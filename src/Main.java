df08fbafcbc88f3080903a882f383f195569a9e3
Jost221 <mr.z.75@mail.ru>
2023-10-11 15:11:17 +0300

    biba

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,12 @@
-import java.util.Random;
+import java.time.LocalDate;
 
 public class Main {
-    private static String generateNumber(int count){
-        String result = new String();
-        for(int i = 0; i < count; i++) result+= String.valueOf(new Random().nextInt(10));
-        return result;
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(minusData(date, 5));
     }
 
-    public static void main(String[] args) {
-        String[] base_mail = {"@mail.ru", "@gmail.com", "@yandex.ru", "@elixirsd.com"};
-        String numbers = "0123456789";
-        String alphavit = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM.";
-
-        // email
-        String email = new String();
-        int len = new Random().nextInt(20);
-        for(int i = 0; i < len; i++){
-            email += new Random().nextInt(2) == 0 ?
-                    alphavit.charAt(new Random().nextInt(alphavit.length())) : numbers.charAt(new Random().nextInt(numbers.length()));
-        }
-        email+= base_mail[new Random().nextInt(base_mail.length)];
-
-        // phone
-        String phone = String.format(
-                "+7 (%s)-%s-%s-%s",
-                generateNumber(3),
-                generateNumber(3),
-                generateNumber(2),
-                generateNumber(2)
-                );
-
-        System.out.println(email);
-        System.out.println(phone);
+    private  static LocalDate minusData(LocalDate date, int day){
+        return date.plusDays(day);
     }
 }
