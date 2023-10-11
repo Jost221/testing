3eb4c304804c72f186c6d2dd65891cb17f2008c6
Jost221 <mr.z.75@mail.ru>
2023-10-11 13:02:40 +0300

    mda

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
-import utils.StringGenerator;
+import java.util.Random;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println(new StringGenerator().GenerateString(10));
+        String[] base_mail = {"@mail.ru", "@gmail.com", "@yandex.ru", "@elixirsd.com"};
+        String numbers = "0123456789";
+        String alphavit = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM.";
+
+        // email
+        String email = new String();
+        int len = new Random().nextInt(20);
+        for(int i = 0; i < len; i++){
+            email += new Random().nextInt(2) == 0 ?
+                    alphavit.charAt(new Random().nextInt(alphavit.length())) : numbers.charAt(new Random().nextInt(numbers.length()));
+        }
+        email+= base_mail[new Random().nextInt(base_mail.length)];
+
+        // phone
+        String phone = "+7 (";
+        while (phone.length() < 18) {
+            switch (phone.length()){
+                case 7:
+                    phone+=")";
+                case 12:
+                    phone+="-";
+                    break;
+                case 15:
+                    phone+="-";
+            }
+            phone+=String.valueOf(new Random().nextInt(10));
+        }
+
+        //
     }
 }
